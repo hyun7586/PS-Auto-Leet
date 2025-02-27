64f4d56212d1d7a8d84799be8d55fd3c6d99f07d
DooHyun Kim <104716006+hyun7586@users.noreply.github.com>
2025-02-27 22:08:23 +0900

    Time: 4 ms (1.2%), Space: 15.5 MB (3.55%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int binary_search(vector<int> nums, int left, int right, int target) {
+        cout<<"binary_search: "<<left<<", "<<right<<'\n';
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (target < nums[mid]) {
+                right = mid - 1;
+            } else if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    int findMinIndex(vector<int> nums) {
+        int temp = nums[0];
+
+        int left = 0, right = nums.size() - 1, mid, result = 0;
+        cout<<"findMinIndex: "<<left<<", "<<right<<'\n';
+
+        while (left <= right) {
+            mid = (left + right) / 2;
+
+            if (temp > nums[mid]) {
+                result = mid;
+                right = mid - 1;
+            } else if (temp <= nums[mid]) {
+                left = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
+    int search(vector<int>& nums, int target) {
+        int specialIndex = findMinIndex(nums);
+        cout<<"specialIndex: "<<specialIndex<<'\n';
+
+        if (specialIndex == 0)
+            return binary_search(nums, 0, nums.size() - 1, target);
+        else {
+            if (target >= nums[0]) {
+                return binary_search(nums, 0, specialIndex - 1, target);
+            } else {
+                cout<<"check\n";
+                return binary_search(nums, specialIndex, nums.size() - 1, target);
+            }
+        }
+    }
+};
