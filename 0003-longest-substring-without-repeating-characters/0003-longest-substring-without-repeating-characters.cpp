6513ddc00465bfcc58380e2605c28b75436253e5
DooHyun Kim <104716006+hyun7586@users.noreply.github.com>
2025-04-03 14:41:09 +0900

    Time: 4 ms (77.45%), Space: 10.7 MB (94.36%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,16 +3,14 @@ public:
     bool visited[256]={false};
 
     int lengthOfLongestSubstring(string s) {
-
         if(s=="")   return 0;
 
         int i=0, j=0, result=0;
         visited[(int)s[i]]=true;
-        for(i=0;i<s.size();i++){
 
-            if(i>0){
-                visited[(int)s[i-1]]=false;
-            }
+        for(i=0;i<s.size();i++){
+            if(i>0) visited[(int)s[i-1]]=false;
+            
             
             while(j<s.size()-1 && !visited[(int)s[j+1]]){
                 j+=1;
